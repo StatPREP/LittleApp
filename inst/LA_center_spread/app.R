a633c76d930c27387b93a32dab26d0420f3af821
Danny Kaplan <kaplan@macalester.edu>
2019-04-04 18:08:22 -0500

    Added ruler to center spread and improved standard-deviation ruler.

--- inst/LA_center_spread/app.R
+++ inst/LA_center_spread/app.R
@@ -24,7 +24,7 @@ choose_stats <-
       hr(),
       checkboxInput("show_ci", "Conf. interval on mean:", value = FALSE) %>%
         tighten(bottom = -10),
-      checkboxInput("show_pred_interval", "Pred. interval", value = FALSE) %>%
+      checkboxInput("show_pred_interval", "Summary interval", value = FALSE) %>%
         tighten(bottom = -10),
       selectInput("interval_level", "Interval  level",
                   choices = list("99%" = 0.99, "95%" = 0.95,
@@ -48,7 +48,9 @@ UI <- function(request) { #it's a function  for bookmarking
       #, bookmarkButton()
     ),
 
-    LA_body()
+    LA_body(plot_widget =
+              plotOutput("main_plot", height = "400px",
+                         brush = brushOpts(id="yruler",  direction  = "y")))
   )
 }
 
@@ -84,6 +86,13 @@ SERVER <- function(input, output, session) {
       req(input$var_y, input$var_x)
       req(the_data$initialized)
 
+      xrange <- c(0, 2) # default for no explanatory variable
+      xvar <- get_explanatory_var()
+      if  (length(xvar)  != 1) {
+        if (get_explanatory_type() == "numeric") xrange = range(xvar)
+        else xrange = c(1, length(unique(xvar)) + 0.5)
+      }
+
       # Make the plot
       the_formula <- as.formula(glue::glue("{input$var_y} ~ {input$var_x}"))
       plot_data  <- get_sample()
@@ -105,15 +114,16 @@ SERVER <- function(input, output, session) {
                                   mean = mean, sd = sd) %>%
           mutate(center = mean, pos_sd = mean + sd, neg_sd =  mean - sd,
                  pos_2sd = mean + 2*sd, neg_2sd =  mean - 2*sd,) %>%
-          select(- mean, - sd,  - center)
+          select( - sd,  - center)
         For_sd_ruler_labels <- For_sd_ruler %>%
-          tidyr::gather(key = label, value = vertical, `pos_sd`, neg_sd, pos_2sd, neg_2sd)
+          tidyr::gather(key = label, value = vertical, `pos_sd`, neg_sd, pos_2sd, neg_2sd, mean)
 
         output$debug_table <- renderTable(For_sd_ruler_labels)
         P <- get_sample()  %>%
           gf_jitter( the_formula, seed = 101,
                      width = 0.10,
                      alpha = 0.5)
+
         if (input$violin) P <- P %>% gf_violin(alpha = 0.2, fill = "blue")
         if (input$show_median) {
           this_formula <- as.formula(glue::glue("median + median ~ {input$var_x}"))
@@ -125,11 +135,13 @@ SERVER <- function(input, output, session) {
         }
         if (input$show_sd) {
           this_formula <- as.formula(glue::glue("`pos_sd` + neg_sd ~ {input$var_x}"))
-          P <- P %>% gf_errorbar(this_formula, data = For_sd_ruler, color = "black", width = 0.1)
-          this_formula <- as.formula(glue::glue("pos_2sd + neg_2sd ~ {input$var_x}"))
-          P <- P %>% gf_errorbar(this_formula, data = For_sd_ruler, color = "black", width = 0.2)
+          P <- P %>% gf_errorbar(this_formula, data = For_sd_ruler, color = "red", width = 0.1)
+          this_formula <- as.formula(glue::glue("pos_2sd + mean ~ {input$var_x}"))
+          P <- P %>% gf_errorbar(this_formula, data = For_sd_ruler, color = "red", width = 0.15)
+          this_formula <- as.formula(glue::glue("neg_2sd + mean ~ {input$var_x}"))
+          P <- P %>% gf_errorbar(this_formula, data = For_sd_ruler, color = "red", width = 0.15)
           this_formula <- as.formula(glue::glue("vertical ~ {input$var_x}"))
-          P <- P %>% gf_text(this_formula, label = ~ label,
+          P <- P %>% gf_text(this_formula, label = ~ label, color = "red",
                              data = For_sd_ruler_labels, nudge_x = 0.1, hjust = 0)
         }
         if (input$show_mean) {
@@ -141,10 +153,16 @@ SERVER <- function(input, output, session) {
            }
         }
         if (input$var_x == "1")
-          P <- P %>% gf_lims(x = c(0,2)) %>% gf_labs(x = "")  %>% gf_theme(no_x_axis)
-
+          P <- P %>% gf_lims(x = c(0,2)) %>%
+          gf_labs(x = "")  %>%
+          gf_theme(no_x_axis) %>%
+          add_y_ruler(x_range = xrange, ruler = input$yruler )
       } else {
         P <- gf_text(1 ~ 1, label = "New data set arriving ...") %>% gf_theme(theme_void())
+      }
+
+      if (get_response_type() == "numeric") { # can't set scale for categorical variable
+        P <- P %>% gf_lims(y = get_y_range())  # to keep scale constant across samples
       }
       return(P)
     })
@@ -165,12 +183,13 @@ SERVER <- function(input, output, session) {
       if (input$var_y == "1")
         return("Not available yet. Select a statistic to display.")
       level <- as.numeric(input$interval_level)
-      the_formula <- get_space_formula()
+      the_formula <- get_frame_formula()
         # as.formula(glue::glue("{input$var_y} ~ {input$var_x}"))
       Stats <- mosaicCore::df_stats(the_formula, data = get_sample(),
+                                    n = length,
                                     mean = mean, mean = mosaicCore::ci.mean(!!level),
                                     median = median, sd = sd,
-                                    prediction = coverage(!!level),
+                                    summary = coverage(!!level),
                                     na.action = "na.pass")
 
       Stats$interval_level <- as.numeric(input$interval_level)
@@ -179,7 +198,7 @@ SERVER <- function(input, output, session) {
         {if (!input$show_ci)  select(., -mean_lower, -mean_upper) else .} %>%
         {if (!input$show_median)  select(.,  -median) else .} %>%
         {if (!input$show_sd) select(.,  -sd) else .} %>%
-        {if (!input$show_pred_interval) select(.,  -prediction_lower,  -prediction_upper) else .}
+        {if (!input$show_pred_interval) select(.,  -summary_lower,  -summary_upper) else .}
 
      Res
     })
