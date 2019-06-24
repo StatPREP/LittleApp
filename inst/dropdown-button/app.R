6dc7b90d6226bb7b96809394ead4dbbd3f7589dd
Danny Kaplan <kaplan@macalester.edu>
2019-06-23 21:39:25 -0500

    Testing out new format

---
+++ inst/dropdown-button/app.R
@@ -0,0 +1,44 @@
+library(shiny)
+library(shinyWidgets)
+
+ui <- fluidPage(
+    dropdownButton(
+        inputId = "mydropdown",
+        label = "Controls",
+        icon = icon("sliders"),
+        status = "primary",
+        circle = FALSE,
+        sliderInput(
+            inputId = "n",
+            label = "Number of observations",
+            min = 10, max = 100, value = 30
+        ),
+        prettyToggle(
+            inputId = "na",
+            label_on = "NAs keeped",
+            label_off = "NAs removed",
+            icon_on = icon("check"),
+            icon_off = icon("remove")
+        )
+    ),
+    tags$div(style = "height: 140px;"), # spacing
+    verbatimTextOutput(outputId = "out"),
+    verbatimTextOutput(outputId = "state")
+)
+
+server <- function(input, output, session) {
+
+    output$out <- renderPrint({
+        cat(
+            " # n\n", input$n, "\n",
+            "# na\n", input$na
+        )
+    })
+
+    output$state <- renderPrint({
+        cat("Open:", input$mydropdown_state)
+    })
+
+}
+
+shinyApp(ui, server)
