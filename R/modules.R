fa36611a0261f033b0591e89f0f227c30f170140
Danny Kaplan <kaplan@macalester.edu>
2019-07-15 19:23:58 -0500

    starting to modularize data selection

---
+++ R/modules.R
@@ -0,0 +1,97 @@
+#' Modules for selecting data
+#'
+#' @export
+select_var_UI <- function(id, label, optional = TRUE) {
+  ns <- NS(id)
+
+  selectizeInput(ns("selector"), label = label, choices = NULL,
+                 options = list(
+                   placeholder = ifelse(optional, "None required", 'Select a variable'),
+                   onInitialize = I('function() { this.setValue(""); }')
+                 ))
+}
+# write filters in terms of x
+#' @export
+select_var <- function(input, output, session, suitable, raw) {
+  var_choices <- reactive({
+    unlist(lapply(req(raw()), suitable))
+    })
+  observe({
+    updateSelectizeInput(session, "selector",
+                      choices = names(raw())[var_choices()])
+  })
+  return(list(var_name = reactive({input$selector})))
+}
+#' @export
+select_data_frame_UI <-
+  function(id,
+           canned = list(Galton = "mosaicData::Galton",
+                         NHANES = "LittleApp::NHANES2"),
+           types = c("numeric", "2 levels", NA, NA)) {
+    ns <- NS(id)
+    tagList(
+      selectInput(ns("frame_name"), "Data source", choices = canned)
+      # ,
+      #             options = list(
+      #               placeholder = 'Select a data frame',
+      #               onInitialize = I('function() { this.setValue(""); }')
+      #             )
+      # )
+    )
+  }
+#' @export
+select_data_frame <- function(input, output, session, ...) {
+  get_raw <- reactive({
+    req(input$frame_name)
+    eval(parse(text = input$frame_name))
+  })
+  max_n <- reactive({nrow(get_raw())})
+  codebook <- reactive({"The codebook will be here"})
+  name <- reactive({input$frame_name})
+
+  list(get_raw = get_raw, max_n = max_n, codebook = codebook, name = name)
+}
+
+
+
+#' @export
+sample_size_UI <- function(id, ...) {
+  ns <- NS(id)
+  samp_n <- selectizeInput(ns("samp_n"), "Sample size n",
+                        choices = c(5, 10, 20, 50, 100, 200, 500, 1000, 2000),
+                        selected = 50)
+  stratify <- checkboxInput(ns("stratify"), label = "Stratify", value = FALSE)
+  dropdown(samp_n, stratify,
+           size = "sm",
+           inputId = ns("n_button"),
+           circle = FALSE,
+           label = "Sample size")
+}
+#' @export
+sample_size <- function(input, output, session, nmax) {
+  possible_sizes <- reactive({
+    population_size = nmax()
+    choices <- c(outer(c(5, 10, 20), c(1, 10, 100, 1000, 10000, 100000)))
+    choices <- as.list(choices[choices < population_size])
+    choices["sampling frame"] <- population_size
+
+    return(choices)
+  })
+  observe({
+    nmax()
+    current_n <- req(input$samp_n)
+    if (!current_n %in% possible_sizes()) current_n <- 50
+    updateSelectizeInput(session, "samp_n",
+                      choices = possible_sizes(),
+                      selected = current_n)
+  })
+
+  # All the relevant inputs are in TMP
+  return(
+    list(
+      n_sample = reactive(input$samp_n),
+      stratify = reactive(input$stratify)
+    )
+  )
+
+}
---
+++ inst/June-2019/app.R
@@ -0,0 +1,83 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinyWidgets)
+library(shinyjs)
+library(ggformula)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    fillPage(
+        # Application title
+        fillRow(p("Little App: Statistical Modeling")),
+        br(),
+
+        #choose_data_frame_UI("get_data"), sample_size_UI("get_N"),
+        br(),
+        fillRow(dropdown( select_data_frame_UI("select_frame"),
+                          select_var_UI("response_name", "response", optional = FALSE),
+                          select_var_UI("explanatory_name", "explanatory"),
+                               inputId = "data_button",
+                               size = "sm",
+                               circle = FALSE,
+                               label = "Data & variables"),
+                sample_size_UI("samp_size"),
+                actionButton("annotations", "Annotations"),
+                prettyToggle("save_plot", label_on = "Dismiss plot",
+                             label_off = "Hold plot",
+                             shape = "curve", outline = TRUE)),
+        br(),
+        hr(),
+
+        conditionalPanel(condition = "! input.save_plot",
+                         plotOutput("full_width", width = "100%")
+        ),
+        conditionalPanel(condition = "input.save_plot",
+                         fillRow(
+                             plotOutput("half_width", width = "100%"),
+                             plotOutput("savedPlot", width = "100%")
+                         )
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    shinyjs::useShinyjs()
+    output$half_width <- renderPlot({ make_plot() })
+    output$full_width <- renderPlot({ make_plot() })
+    output$savedPlot <- renderPlot({
+        if (input$save_plot)  make_plot()
+        else NULL
+        })
+    make_plot <- reactive({
+        gf_density( ~ waiting, data = faithful, fill = 'darkgray', color = 'white')
+    })
+
+    RAW <- callModule(select_data_frame, "select_frame")
+    RESPONSE <- callModule(select_var, "response_name",
+                              suitable = function(x) TRUE,
+                              raw = RAW$get_raw)
+    EXPLANATORY <- callModule(select_var, "explanatory_name",
+                           suitable = function(x) is.numeric(x),
+                           raw = RAW$get_raw)
+    N <- callModule(sample_size, "samp_size", nmax = RAW$max_n)
+    observe({
+    cat("Selected frame is", RAW$name(), "\n")
+    })
+    observe({
+        cat("Response var is", EXPLANATORY$var_name(), "\n")
+    })
+
+
+    }
+
+# Run the application
+shinyApp(ui = ui, server = server)
