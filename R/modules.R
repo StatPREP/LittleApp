daae12e4aa2996da3fceb38327b7b8c18ef832fc
Danny Kaplan <kaplan@macalester.edu>
2019-07-19 08:32:10 -0500

    Added in plot splitting.

--- R/modules.R
+++ R/modules.R
@@ -1,5 +1,29 @@
 #' Modules for selecting data
 #'
+#' Does something
+#'
+#'
+#'
+#'
+#'
+#' @export
+two_plots_UI <- function(id) {
+  ns <- NS(id)
+  tagList(
+     plotOutput(ns("current_plot"), width = "100%"),
+     plotOutput(ns("saved_plot"), width = "100%")
+  )
+}
+#' @export
+two_plots <- function(input, output, session, data, annots, display) {
+  observe({
+    DF <- data$get_raw()
+    sample <- data$get_sample()
+    cat("In two_plots\n")
+    output$current_plot <- renderPlot({plot(1:10)})
+    output$saved_plot <- renderPlot({plot(1:20)})
+  })
+}
 #'
 #' @export
 select_var_UI <- function(id, label, optional = TRUE, show = TRUE) {
@@ -20,9 +44,10 @@ select_var <- function(input, output, session, suitable, raw, value) {
     })
   observe({
     choices <- var_choices()
+    isolate(
     updateSelectizeInput(session, "selector",
                       choices = names(raw())[choices],
-                      selected = value)
+                      selected = ifelse(input$selector == "", value, input$selector)))
   })
   return(
     list(
@@ -38,29 +63,65 @@ select_data_frame_UI <-
                          NHANES = "LittleApp::NHANES2"),
            types = c("numeric", "2 levels", NA, NA)) {
     ns <- NS(id)
-    tagList(
-      selectizeInput(ns("frame_name"), "Data source", choices = canned,
-                     width = "150px",
-                     options = list(
-                       placeholder = 'Select a data frame',
-                       onInitialize = I('function() { this.setValue(""); }')
-                     )
-      ),
-      select_var_UI(ns("response_name"), "response var", optional = FALSE),
-      select_var_UI(ns("explanatory_name"), "explanatory var"),
-      select_var_UI(ns("covar"), "second explanatory var", show = FALSE),
-      select_var_UI(ns("covar2"), "third explanatory var", show = FALSE),
-      sample_size_UI(ns("samp_size"))
-    )
 
+    list(
+      dropdown(
+        inputId = "data_button",
+        size = "sm",
+        circle = FALSE,
+        label = "Data & variables",
+        width = "180px",
+
+        selectizeInput(ns("frame_name"), "Data source", choices = canned,
+                       width = "150px",
+                       options = list(
+                         placeholder = 'Select a data frame',
+                         onInitialize = I('function() { this.setValue(""); }')
+                       ) %>% tighten()
+        ),
+        select_var_UI(ns("response_name"), "response var", optional = FALSE),
+        select_var_UI(ns("explanatory_name"), "explanatory var"),
+        select_var_UI(ns("covar"), "second explanatory var", show = FALSE),
+        select_var_UI(ns("covar2"), "third explanatory var", show = FALSE)
+      ),
+      dropdown(
+        inputId = "sample_button",
+        size = "sm",
+        circle = FALSE,
+        label = "Sampling",
+        width = "180px",
+        selectInput(ns("samp_n"), "Sample size n", width = "150px",
+                    choices = c(5, 10, 20, 50, 100, 200, 500, 1000, 2000),
+                    selected = 50),
+        stratify <- checkboxInput(ns("stratify"), label = "Stratify", value = FALSE)
+      )
+    )
   }
 #' @export
-select_data_frame <- function(input, output, session, state, ...) {
+select_data_frame <- function(input, output, session, state, new_sample,...) {
   max_n <- reactive({nrow(get_raw())})
 
   get_raw <- reactive({
-    req(input$frame_name)
+    input$frame_name
+    cat("Accessing raw data\n")
     eval(parse(text = input$frame_name))
+  })
+
+  possible_sizes <- reactive({
+    population_size = nrow(get_raw())
+    choices <- c(outer(c(5, 10, 20), c(1, 10, 100, 1000, 10000, 100000)))
+    choices <- as.list(choices[choices < population_size])
+    choices["sampling frame"] <- population_size
+
+    return(choices)
+  })
+
+  observe({
+    current_n <- req(input$samp_n)
+    if (!current_n %in% possible_sizes()) current_n <- 50
+    updateSelectInput(session, "samp_n",
+                      choices = possible_sizes(),
+                      selected = current_n)
   })
 
   codebook <- reactive({"The codebook will be here"})
@@ -79,72 +140,46 @@ select_data_frame <- function(input, output, session, state, ...) {
   COVAR2 <- callModule(select_var, "covar2",
                        suitable = function(x) FALSE,
                        raw = get_raw, value = state$covar2)
-  N <- callModule(sample_size, "samp_size", nmax = max_n)
 
+  get_sample <- reactive({
+    variables <- var_names()
+    req(variables["response"] != "")
+    req(input$samp_n)
+    new_sample() # for the dependency
+    cat("Getting sample\n")
+    # Need to add in stratification logic
 
+    get_raw()[variables] %>%
+      dplyr::sample_n(size = input$samp_n) %>%
+      setNames(names(variables)) # standardize to response, explanatory, covar, covar2
+  })
 
+  # The selected variables
   var_names <- reactive({
     c(response = RESPONSE$var_name(), explanatory = EXPLANATORY$var_name(),
       COVAR$var_name(), explanatory = COVAR2$var_name())
   })
 
+  # The possibilities for each of the variables
   var_choices <- reactive({
     list(response = RESPONSE$var_choices(),
          explanatory = EXPLANATORY$var_choices(),
          covar = COVAR$var_choices(),
          covar2 = COVAR2$var_choices(),
-         n_sample = N$n_sample()
     )
   })
 
   return(
     list(get_raw = get_raw,
+         get_sample = get_sample,
          max_n = max_n,
          codebook = codebook,
          name = name,
          var_names = var_names,
-         var_choices = var_choices)
+         var_choices = var_choices,
+         new_sample = reactive(input$new_sample))
   )
 }
 
 
 
-#' @export
-sample_size_UI <- function(id, ...) {
-  ns <- NS(id)
-  tagList(
-  samp_n <- selectInput(ns("samp_n"), "Sample size n", width = "150px",
-                        choices = c(5, 10, 20, 50, 100, 200, 500, 1000, 2000),
-                        selected = 50),
-  stratify <- checkboxInput(ns("stratify"), label = "Stratify", value = FALSE)
-  )
-}
-#' @export
-sample_size <- function(input, output, session, nmax) {
-
-  possible_sizes <- reactive({
-    population_size = nmax()
-    choices <- c(outer(c(5, 10, 20), c(1, 10, 100, 1000, 10000, 100000)))
-    choices <- as.list(choices[choices < population_size])
-    choices["sampling frame"] <- population_size
-
-    return(choices)
-  })
-  observe({
-    nmax()
-    current_n <- req(input$samp_n)
-    if (!current_n %in% possible_sizes()) current_n <- 50
-    updateSelectInput(session, "samp_n",
-                      choices = possible_sizes(),
-                      selected = current_n)
-  })
-
-  # All the relevant inputs are in TMP
-  return(
-    list(
-      n_sample = reactive(input$samp_n),
-      stratify = reactive(input$stratify)
-    )
-  )
-
-}
--- inst/June-2019/app.R
+++ inst/June-2019/app.R
@@ -6,6 +6,35 @@ library(shinyWidgets)
 library(shinyjs)
 library(ggformula)
 
+#  Set the controls here
+Control_menu <-  dropdown(
+  inputId = "annotations",
+  size = "sm",
+  circle = FALSE,
+  label = "Controls",
+  width = "180px",
+  actionButton("do_something", "Do something!")
+)
+Control_menu2 <- NULL # A slot for another control menu if you want it
+
+# Boilerplate
+Tmp <- select_data_frame_UI("select_frame")
+Data_menu <- Tmp[[1]]
+Sample_menu <- Tmp[[2]]
+New_sample_button <-
+  actionButton("new_sample", "New sample", icon=icon("child"),
+               style = "color: white; background-color: green; font-size: 16px; margin-left: 0px;")
+
+
+History_menu <-  dropdown(
+    inputId = "history_button",
+    size = "sm",
+    circle = FALSE,
+    label = "History",
+    width = "180px",
+    checkboxInput("show_both", label = "Show history", value = FALSE),
+    checkboxInput("freeze", label = "Freeze history", value = FALSE)
+  )
 # Define UI for application that draws a histogram
 ui <-
     function(request) {
@@ -17,29 +46,14 @@ ui <-
 
                 #choose_data_frame_UI("get_data"), sample_size_UI("get_N"),
                 br(),
-                fillRow(
-                  dropdown(
-                    select_data_frame_UI("select_frame"),
-
-                    inputId = "data_button",
-                    size = "sm",
-                    circle = FALSE,
-                    label = "Data & variables",
-                    width = "150px"
-                  ),
-
-                  actionButton("annotations", "Annotations"),
-                  prettyToggle("save_plot", label_on = "Dismiss plot",
-                               label_off = "Hold plot",
-                               shape = "curve", outline = TRUE)
-                ),
+                fillRow(Data_menu, br(), New_sample_button, Sample_menu, br(), Control_menu, Control_menu2, History_menu),
                 br(),
                 hr(),
 
-                conditionalPanel(condition = "! input.save_plot",
+                conditionalPanel(condition = "! input.show_both",
                                  plotOutput("full_width", width = "100%")
                 ),
-                conditionalPanel(condition = "input.save_plot",
+                conditionalPanel(condition = "input.show_both",
                                  fillRow(
                                      plotOutput("half_width", width = "100%"),
                                      plotOutput("savedPlot", width = "100%")
@@ -51,73 +65,65 @@ ui <-
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-    state <- list()
-    state$frame <- "mosaicData::Galton"
-    state$resp <- "father"
-    state$explan <- "mother"
-    state$covar <- ""
-    state$covar2 <- ""
-    state$n <- 20
+  shinyjs::useShinyjs()
+  state <- list()
+  state$frame <- "mosaicData::Galton"
+  state$resp <- "father"
+  state$explan <- "mother"
+  state$covar <- ""
+  state$covar2 <- ""
+  state$n <- 20
 
-    observe({
-      query <- parseQueryString(session$clientData$url_search)
-      for (name in names(query))
-        state[name] <<- query[[name]]
+  # Placeholder for the history
+  current_plot <- previous_plot <- gf_text(1 ~ 1, label = "No history yet.")
 
-      state["n"] <<- as.integer(state[["n"]])
+  initialized = FALSE
+  observe({
+    query <- parseQueryString(session$clientData$url_search)
+    for (name in names(query))
+      state[name] <<- query[[name]]
 
-      # I don't know why I have to do this
-      foo <- force(state$n)
-      # Do this here so that the variable selectors have a chance to be set up.
+    state["n"] <<- as.integer(state[["n"]])
+
+    # I don't know why I have to do this
+    foo <- force(state$n)
+    # Do this here so that the variable selectors have a chance to be set up.
+    if (!initialized) {
       updateSelectizeInput(session, inputId = "select_frame-frame_name",
                            selected = state$frame)
       updateSelectInput(session, inputId = "select_frame-samp_size-samp_n",
                         selected = foo)
-      }, priority=1000)
-
-
-
-
-    shinyjs::useShinyjs()
-
-    onBookmark(function(state) {
-        cat("Starting the bookmark.\n")
-        browser()
-        state$values$data <- RAW$var_choices()
-    })
-
-    onRestore(function(state) {
-        browser()
-        cat("State names are:", paste(names(state), collapse = ", "), "\n")
-    })
-
-    onRestored(function(state) {
-
-        browser()
-    })
-
-    output$half_width <- renderPlot({ make_plot() })
-    output$full_width <- renderPlot({ make_plot() })
-    output$savedPlot <- renderPlot({
-        if (input$save_plot)  make_plot()
-        else NULL
-        })
-    make_plot <- reactive({
-        gf_density( ~ waiting, data = faithful, fill = 'darkgray', color = 'white')
-    })
-
-    RAW <- callModule(select_data_frame, "select_frame", state = state)
-
-
-
-    observe({
-    cat("Selected frame is", RAW$name(), "\n")
-        cat("Variable names are", paste(RAW$var_names(), collapse = ", "), "\n")
-    })
-
-
+      initialize = TRUE
     }
+  }, priority=1000)
+
+
+  # update the plots as needed
+  observe({
+    newplot <- make_plot()
+
+    # Make freeze an action button and copy the current plot whenever it's pressed.
+    # This will need to be in a separate observer()
+
+    if (! isolate(input$freeze)) previous_plot <<- current_plot
+    current_plot <<- newplot
+
+    output$half_width <- renderPlot({ current_plot })
+    output$full_width <- renderPlot({ current_plot })
+    output$savedPlot <- renderPlot({previous_plot})
+  })
+
+
+  make_plot <- reactive({
+    tmp <- RAW$get_sample()
+    vnames <- RAW$var_names()
+    gf_jitter(response ~ explanatory, data = tmp, color = "black") %>%
+      gf_labs(y = vnames["response"], x = vnames["explanatory"])
+  })
+
+  RAW <- callModule(select_data_frame, "select_frame", state = state,
+                    new_sample = reactive(input$new_sample))
+}
 
 # Run the application
-#enableBookmarking(store = "url")
 shinyApp(ui = ui, server = server)
